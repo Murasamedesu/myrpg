82a036ce7962122f1e3c8d242913f1c3cd11cec2
Murasamedesu <murasamedesu@qq.com>
2026-06-30 15:43:14 +0800

    首次修改1-2

---
+++ mymmorpg.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 80043
+ Source Host           : localhost:3306
+ Source Schema         : mymmorpg
+
+ Target Server Type    : MySQL
+ Target Server Version : 80043
+ File Encoding         : 65001
+
+ Date: 30/06/2026 13:23:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
