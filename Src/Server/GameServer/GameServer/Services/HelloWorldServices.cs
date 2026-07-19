c6237f16c13c0623a29fbcdf32880a4cd5a552f4
Murasamedesu <murasamedesu@qq.com>
2026-07-19 13:52:47 +0800

    test xieyi

---
+++ Src/Server/GameServer/GameServer/Services/HelloWorldServices.cs
@@ -0,0 +1,42 @@
+﻿
+using Common;
+using Network;
+using SkillBridge.Message;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Services
+{
+    class HelloWorldServices:Singleton<HelloWorldServices>
+    {
+        
+        public void Init()
+        {
+            
+        }
+
+
+
+        public void Start()
+        {
+            MessageDistributer<NetConnection<NetSession>>.Instance.Subscribe<FirstTestRequest>(this.OnFirstTestRequest);
+        }
+
+
+        void OnFirstTestRequest(NetConnection<NetSession> sender, FirstTestRequest request)
+        {
+            Log.InfoFormat("FirstTestRequest: message:{0}", request.Helloworld);
+        }
+
+        public void Stop() 
+        { 
+
+
+
+        }
+
+    }
+}
